39f2ba9d1e2ff691b9527a3191fe2a23388a4728
vagrant <vagrant@student-dev-box>
2014-03-27 21:52:25 +0000

    tests are greenie

--- my_hash/my_hash.rb
+++ my_hash/my_hash.rb
@@ -1,13 +1,21 @@
 class MyHash
   def initialize
+    @array = Array.new(64)
+
   end
 
   def find_index(key)
+    index = key.hash%64
+    index
   end
 
   def []=(key, value)
+    index = key.hash%64
+    @array[index] = value
   end
 
   def [](key)
+    index = key.hash%64
+    @array[index]
   end
 end
